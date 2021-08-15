75ad79f4d8b3287b2d3d16c00cf987811a173646
Nicolas Andrew Burnett <nicolasandrewburnett@gmail.com>
2021-08-15 17:48:52 -0500

    Added basic Navbar & linter/formatter

---
+++ components/Navigation.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import TerminalSVG from './svg/TerminalSVG'
+import styles from '../styles/component/Navigation.module.scss'
+import { version } from '../package.json'
+
+export default function Navigation() {
+
+  return (
+    <div id={styles.navigation}>
+      <Link href='/'>
+        <div id={styles.logo}>
+          <TerminalSVG />
+          <div>v{version}</div>
+        </div>
+      </Link>
+    </div>
+  )
+}
---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import Navigation from '../components/Navigation'
+import type { AppProps } from 'next/app'
+
+function Application({ Component, pageProps }: AppProps) {
+  return (
+    <div id='website'>
+      <Navigation />
+      <Component {...pageProps} />
+    </div>
+    )
+}
+export default Application
